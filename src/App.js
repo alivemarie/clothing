c0bf831fda167bfa49170b7dd5a40610e9a9bb38
Eva <alivemarie@gmail.com>
2021-07-26 15:18:49 +0300

    added header component with links, input and button components, and styles for them

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 function App() {
   return (
     <div>
+        <Header/>
         <Switch>
             <Route exact path='/' component={HomePage}/>
             <Route path='/shop' component={ShopPage}/>
+            <Route path='/signin' component={SignInAndSignUp}/>
         </Switch>
     </div>
   );
