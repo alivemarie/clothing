953ff208d5b10fd5e4e6e3c978b2a8b918a252aa
Eva <alivemarie@gmail.com>
2021-08-16 12:29:11 +0300

    added cart-dropdown modal window

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import {connect} from "react-redux";
 import {auth} from "../../firebase/firebase.utils";
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import CartIcon from "../cart-icon/cart-icon";
+import CartDropdown from "../cart-dropdown/cart-dropdown";
 import './header.scss'
 
 const Header = ({currentUser}) => (
@@ -22,6 +23,7 @@ const Header = ({currentUser}) => (
             }
             <CartIcon/>
         </div>
+        <CartDropdown/>
     </div>
 )
 
